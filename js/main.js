50c3b2540ffa577f3c082dcdb9b06d8be2606690
Osvaldo Josué <osvaldojosuecruz@gmail.com>
2023-11-29 08:37:52 -0300

    Eliminación de alert y prompt

--- js/main.js
+++ js/main.js
@@ -1,32 +1,64 @@
 
 let puntaje = 0;
 let form = document.getElementById("formParticipante");
-
+const planetas = ["Mercurio", "Venus", "Tierra", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno"];
+const preguntas = {
+    "respuesta1": [
+        "c", //respuesta correcta
+        "pregunta1", //Contenedor de la pegunta
+        "pregunta2", //contenedor siguiente
+        "A pesar de las ilustraciones en amarillo a las que estamos acostumbrados el color del sol es blanco, de otra manera las nubes, la nieve y los osos polares serian amarillos. El motivo del por qué lo vemos amarillo es por la cantidad de gases y polvo que se acumulan en la atmósfera y dispersan la luz volviendo de ese color el sol durante el atardecer."
+    ],
+    "respuesta2": [
+        "c",
+        "pregunta2",
+        "pregunta3",
+        "Plutón fue considerado durante mucho tiempo el noveno planeta de nuestro sistema solar. Hasta 2006 cuando se redefinió lo que significa ser un planeta y en vista de los nuevos requisitos quedó fuera de los otros 8 con la categoría de planeta menor."
+    ],
+    "respuesta3": [
+        "a",
+        "pregunta3",
+        "pregunta4",
+        "Desde el 8 de junio del 2021, se reconoce al Océano Austral como el quinto océano del mundo. Conocido extraoficialmente por muchos años este oceano es el cuerpo de agua que rodea la Antártida."
+    ],
+    "respuesta4": [
+        "b",
+        "pregunta4",
+        "pregunta5",
+        `Después el sol el orden es : ${planetas.join(", ")}.`
+    ],
+    "respuesta5": [
+        "b",
+        "pregunta5",
+        "resumen",
+        "Aunque Mercurio se encuentra más cerca, Venus tiene una atmósfera compuesta por un 96% dióxido de carbono, este gas en exceso atrapa el calor que viene del sol produciendo que el planeta tenga 3 veces la temperatura de Mercurio."
+    ]
+}
 class Persona {
     constructor(nombre, pais) {
         this.nombre = nombre;
         this.pais = pais;
     }
-    verPuntaje() {
-        alert(`${this.nombre} tu puntaje es ${puntaje}`);
-    }
-
-
 }
 
-
-
-
 window.addEventListener("load", ordenarDivs());
-
 function ordenarDivs() {
     let datosLocalStorage = JSON.parse(localStorage?.getItem("participante"));
-    if (datosLocalStorage?.nombre) {
+    if (!datosLocalStorage?.nombre) {
         document.getElementById('login').setAttribute("hidden", false);
         document.getElementById('opciones').style.display = 'block';
+        document.getElementById('test').style.display = 'none';
     } else {
-        document.getElementById('login').removeAttribute("hidden");
-        document.getElementById('opciones').style.display = 'none';
+
+        document.getElementById('login').setAttribute("hidden", false);
+        document.querySelector("#aviso h3").innerText = `Estimado ${datosLocalStorage?.nombre}`;
+        document.querySelector("#aviso p").innerText = "Según nuestros registros, ya se encuentra inscrito. ¿Desea incribir a otra persona?"
+        document.getElementById("cerrar").innerText = "Cerrar sesión e incribir a otra persona";
+        document.getElementById("aviso").showModal();
+        document.getElementById("cerrar").addEventListener("click", () => {
+            localStorage.clear();
+            document.getElementById('opciones').style.display = 'block';
+        });
     }
 }
 
@@ -39,11 +71,16 @@ form.addEventListener("submit", (e) => {
     const participante = new Persona(nombre, pais);
     const participanteJSON = JSON.stringify(participante);
     localStorage.setItem("participante", participanteJSON);
-    // alert(`Bienvenido ${participante.nombre}!!`);
-    ordenarDivs();
-
+    document.querySelector("#aviso h3").innerText = `Estimado ${nombre}`;
+    document.querySelector("#aviso p").innerText = "Gracias por tu participación, en los proximos dias nos comunicaremos con usted para hacer las gestión de la matricula"
+    document.getElementById("cerrar").innerText = "Cerrar";
+    document.getElementById("aviso").showModal();
+    document.getElementById("cerrar").addEventListener("click", () => {
+        ordenarDivs();
+    });
 
 });
+
 document.getElementById("verPuntaje").addEventListener("click", () => {
 
     let datosLocalStorage = JSON.parse(localStorage?.getItem("participante"));
@@ -51,78 +88,55 @@ document.getElementById("verPuntaje").addEventListener("click", () => {
     participante.verPuntaje();
 });
 
+function iniciar() {
 
-function iniciarTest() {
+    document.getElementById('info').style.display = 'none';
+    document.getElementById('test').style.display = 'block';
+    document.getElementById("pregunta1").style.display = 'block';
+}
+let radio = document.getElementsByClassName("radio");
 
-    const planetas = ["Mercurio", "Venus", "Tierra", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno"];
-    let preguntas = [
-        {
-            pregunta: "¿De qué color es el sol?",
-            opciones: "A : Amarillo \nB : Rojo\nC : blanco",
-            respuestaCorrecta: "C",
-            mensaje:
-                "\n\nA pesar de las ilustraciones en amarillo a las que estamos acostumbrados el color del sol es blanco, de otra manera las nubes, la nieve y los osos polares serian amarillos. El motivo del por qué lo vemos amarillo es por la cantidad de gases y polvo que se acumulan en la atmósfera y dispersan la luz volviendo de ese color el sol durante el atardecer.",
-        },
-        {
-            pregunta: "¿Cuántos planetas hay actualmente en nuestro sistema solar?",
-            opciones: `A : 10 \nB : 9 \nC :  ${planetas.length}`,
-            respuestaCorrecta: "C",
-            mensaje:
-                "\n\nPlutón fue considerado durante mucho tiempo el noveno planeta de nuestro sistema solar. Hasta 2006 cuando se redefinió lo que significa ser un planeta y en vista de los nuevos requisitos quedó fuera de los otros 8 con la categoría de planeta menor.",
-        },
-        {
-            pregunta: "¿Cuantos oceanos hay en la tierra?",
-            opciones: "A : 5\nB : 3\nC : 4",
-            respuestaCorrecta: "A",
-            mensaje:
-                "\n\nDesde el 8 de junio del 2021, se reconoce al Océano Austral como el quinto océano del mundo. Conocido extraoficialmente por muchos años este oceano es el cuerpo de agua que rodea la Antártida.",
-        },
-        {
-            pregunta: "¿Cual es el planeta que está más cerca del sol?",
-            opciones: "A : Marte\nB : Mercurio\nC : Venus",
-            respuestaCorrecta: "B",
-            mensaje: `\n\nDespués el sol el orden es : ${planetas.join(", ")}.`,
-        },
-        {
-            pregunta: "¿Cual es el planeta más caliente del sistema solar?",
-            opciones: "A : Mercurio\nB : Venus\nC : Neptuno",
-            respuestaCorrecta: "B",
-            mensaje:
-                "\n\nAunque Mercurio se encuentra más cerca, Venus tiene una atmósfera compuesta por un 96% dióxido de carbono, este gas en exceso atrapa el calor que viene del sol produciendo que el planeta tenga 3 veces la temperatura de Mercurio.",
-        },
-    ];
+for (let i = 0; i < radio.length; i++) {
+    radio[i].addEventListener("click", function (event) {
+        let alternativa = event.target;
+        evaluar(alternativa.value, alternativa.name);
+    })
+}
 
-    // Funcion de orden superior para hacer las preguntas, evaluar respuestas y sumar el puntaje obtenido.
-    preguntas.forEach((p) => {
-        let respuesta;
-        while ((respuesta != "A") && (respuesta != "B") && (respuesta != "C")) {
-            // Guarda la respuesta en mayuscula
-            respuesta = prompt(`Responde con la letra de cada alternativa\n\n ${p.pregunta} \n\n${p.opciones}`).toUpperCase();
+function evaluar(respuesta, i) {
 
-            // Si la respuesta es cualquier otra letra o numero, repite la pregunta
-            if (respuesta != "A" && respuesta != "B" && respuesta != "C") {
-                alert("Respuesta inválida, intenta de nuevo.")
-            } else if (respuesta == p.respuestaCorrecta) {// Si es la respuesta correcta muestra el mensaje de exito, guarda y suma los puntos. 
-                puntaje += 2;
-                alert("¡Correcto!\n\nTu puntaje es : " + puntaje + p.mensaje)
-            } else { // Si no, muestra el mensaje de error con un mensaje del por qué
-                alert(`Respuesta incorrecta \n\nEs la alternativa  ${p.respuestaCorrecta}  ${p.mensaje}`)
 
-            }
+    if (respuesta == preguntas[i][0]) {
+        puntaje += 2;
+        clase = "success";
+        mensaje = "Correcto!";
+    } else {
+        clase = "error";
+        mensaje = "Incorrecto!";
+    }
+
+    document.getElementById(preguntas[i][1]).style.display = 'none';
+    document.getElementById("mensaje").innerHTML = `<div role="alert" class="alert alert-${clase} py-5">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>${mensaje} <p>${preguntas[i][3]}</p></span> <div> 
+        <button id="siguiente" class="btn btn-md ">Siguiente</button>
+        </div></div>`;
+    document.getElementById("siguiente").addEventListener("click", () => {
+        document.getElementById("mensaje").innerHTML = "";
+        if (preguntas[i][2] == "resumen") {
+            document.getElementById(preguntas[i][2]).innerHTML = ` <h3 class="text-2xl py-5 card-title">Obtuviste ${puntaje} pts, por lo tanto tienes un ${10 * puntaje}% de descuento en la matricula </h3> <div class="form-control mt-6">
+            <button class="btn glass" onclick="inscripcion()">Incribirme</button>
+        </div>`;
         }
+        document.getElementById(preguntas[i][2]).style.display = 'block';
     });
 
-
-    // Si termina el cuestionario y no alcanza el puntaje necesario para el premio
-    if (puntaje < 10) {
-        alert("No conseguiste los 10 puntos para el premio, intenta de nuevo!");
-        puntaje = 0;
-    }
-    // Si el puntaje llega a 10, se termina y muestra el mensaje de exito 
-    if (puntaje >= 10) {
-        alert(`¡Felicitaciones  ${participante.nombre}!\n\n¡Has ganado un viaje al espacio en Estación Espacial Internacional!`);
-        puntaje = 0;
-    }
+}
 
 
-} 
+function inscripcion(params) {
+
+    document.getElementById('login').removeAttribute("hidden");
+    document.getElementById('mensajeInscripcion').innerText = `Regístrate ${10 * puntaje}% de descuento por tu participación en el test de nivelación!`;
+    document.getElementById('opciones').style.display = 'none';
+}
